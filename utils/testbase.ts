1fbcadf455bd88ddb81d732849347a98202585bd
RavanSeyfulla <ravanseyfullayev@gmail.com>
2025-06-06 11:43:21 -0400

    Update testbase.ts

--- utils/testbase.ts
+++ utils/testbase.ts
@@ -1,10 +1,13 @@
 import { test as base } from "@playwright/test";
 import testData from "./testData.json";
 
+// Defining a custom fixture type with a 'data' field based on the JSON structure
+type MyFixtures = {
+  data: typeof testData;
 type MyFixtures = {
   data: typeof testData;
 };
-
+// Extending the base test with our custom fixture
 export const fixture = base.extend<MyFixtures>({
-  data: testData,
+  data: testData, // Providing 'data' fixture to be used in tests
 });
