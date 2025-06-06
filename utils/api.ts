824c4a3e14fc582289188d5b177904468e0237ec
RavanSeyfulla <ravanseyfullayev@gmail.com>
2025-06-06 11:46:32 -0400

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -1,13 +1,18 @@
 import { APIRequestContext, expect } from "@playwright/test";
 
+// Creating a class to manage API actions
 export class api {
+  // Storing API request context (used to make HTTP requests)
   readonly apiContext: Promise<APIRequestContext>;
-
+  
+// Constructor receives the API context (created outside this class)
   constructor(apiContext: Promise<APIRequestContext>) {
     this.apiContext = apiContext;
   }
+
+   // Method to send a POST request to the wishlist API
   async wishlist(productDetails: object, endpointUrl: string) {
-    //api request to add wish list
+    // Sending the request with product data
     const response = await (
       await this.apiContext
     ).post(endpointUrl, {
@@ -18,6 +23,7 @@ export class api {
     expect(response.ok()).toBeTruthy();
   }
 
+  // Method to send a POST request to the add-to-cart API
   async addToCart(product: object, endpoint: string) {
     const response = await (
       await this.apiContext
